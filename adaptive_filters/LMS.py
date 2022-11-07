56c3451ea17fc207fc5a2fbe94ba14f7da42a7a2
Michel Hardenberg <108325720+MHardenberg@users.noreply.github.com>
2022-11-07 10:51:14 +0100

    Add files via upload

--- adaptive_filters/LMS.py
+++ adaptive_filters/LMS.py
@@ -1,7 +1,7 @@
 import numpy as np
 from adaptive_filters.AdaptiveFilter import AdaptiveFilter
 
-class normLMS(AdaptiveFilter):
+class LMS(AdaptiveFilter):
     ''' normalized LMS algorith class.
     Signature:
 normLMS(
@@ -17,15 +17,11 @@ Parameters
 signal : numpy.ndarray
 desired_signal : numpy.ndarray
 weights : numpy.ndarray
-
-kwargs:
-mu_heuristic : float
-    Heuristic step size for ensuring under-damped conditions. Default = .05
-psi : float
-    Division by zero guard. Default = .001
+step_size : float
 
 Methods
 -------
+
 compute_correction(self, signal_vectorized, error)
 
 Inherited from AdaptiveFilter:
@@ -36,7 +32,8 @@ adapt_weights()
     Runs algorithm through all data points
 
 -------'''
-    def __init__(self, signal, desired_signal, weights, **kwargs):
+
+    def __init__(self, signal, desired_signal, weights, step_size):
         self.signal: np.ndarray = signal
         self.desired_signal: np.ndarray = desired_signal
         self.samples: int = len(signal)
@@ -45,20 +42,10 @@ adapt_weights()
         self.weights: np.ndarray = np.zeros((self.filter_order, self.samples))
         self.weights[:, 0] = weights
 
-        if 'mu_heuristic' in kwargs:
-            self.heuristic_step_size = kwargs['mu_heuristic']
-        else:
-            self.heuristic_step_size = .05
-
-        if 'psi' in kwargs:
-            self.zero_div_guard : float = kwargs['psi']
-        else:
-            self.zero_div_guard : float = .001
+        self.step_size: float = step_size
 
         self.estimate: np.ndarray = np.zeros(signal.shape)
         self.error: np.ndarray = np.zeros(self.samples - self.filter_order)
 
-
     def compute_correction(self, signal_vectorized, error):
-        return 2 * self.heuristic_step_size /\
-               (signal_vectorized @ signal_vectorized + self.zero_div_guard) * error * signal_vectorized
+        return 2 * self.step_size * signal_vectorized * error
